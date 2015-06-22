b22da50da373eae020053a2316dba765484015d5
Gianpaolo Macario <gmacario@gmail.com>
2015-06-22 16:41:19 +0200

    Also compile hello-thread from sources

--- runme2.sh
+++ runme2.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -x
 set -e
@@ -6,16 +6,34 @@ set -e
 SYSTEMD_REPOSITORY=https://github.com/gmacario/systemd.git
 #SYSTEMD_BRANCH=bootchart-hackme
 SYSTEMD_BRANCH=fix-issue139
+#SYSTEMD_BRANCH=v220
 
 dnf upgrade -y
+
+# -------------------------------------------------------------------
+# Install prerequisite packages
+# -------------------------------------------------------------------
 
 # Prereq for getting systemd sources
 dnf install -y git
 # Prereq for building systemd
-dnf install -y gperf intltool libcap-devel libgcrypt-devel libmount-devel libtool 
+dnf install -y glib2-devel gperf intltool libcap-devel libgcrypt-devel libmount-devel libtool 
 dnf install -y libxslt docbook-style-xsl make
 # Additional tools for debugging et al.
-dnf install -y gdb openssh-clients tig
+dnf install -y gdb openssh-clients procps-ng psmisc tig
+
+# -------------------------------------------------------------------
+# Compile testcase for systemd issue 139
+# -------------------------------------------------------------------
+
+cd /shared
+[ ! -e hello-thread ] && git clone https://github.com/gmacario/hello-thread
+cd /shared/hello-thread
+make build-native
+
+# -------------------------------------------------------------------
+# Compile systemd from sources
+# -------------------------------------------------------------------
 
 cd /shared
 [ ! -e systemd ] && git clone ${SYSTEMD_REPOSITORY}
@@ -33,9 +51,22 @@ git checkout ${SYSTEMD_BRANCH}
 
 make
 
-# Test
+# -------------------------------------------------------------------
+# Test: Run systemd-bootchart
+# -------------------------------------------------------------------
+
+(/shared/hello-thread/hello-thread &>/tmp/hello-thread.$$) &
+sleep 10
+ps ax -L
+
 mkdir -p /run/log
-./systemd-bootchart --rel --freq=50 --samples=1000 --scale-x=100 --scale-y=20 --cmdline --per-cpu
-cp /run/log/*.svg /shared/
+#./systemd-bootchart --rel --freq=50 --samples=1000 --scale-x=100 --scale-y=20 --cmdline --per-cpu
+/shared/systemd/systemd-bootchart --rel --freq=50 --samples=1000 --scale-x=100 --scale-y=20 --cmdline
+
+ps ax -L
+killall hello-thread
+
+# Export SVG
+mv /run/log/*.svg /shared/
 
 # EOF
